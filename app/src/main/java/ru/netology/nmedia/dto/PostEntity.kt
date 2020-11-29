5a710ac368b164bdaefcc9e52746b4c5749f6631
NoXis <63115274+NoXis82@users.noreply.github.com>
2020-11-29 22:03:37 +0300

    migrate to Room

---
+++ app/src/main/java/ru/netology/nmedia/dto/PostEntity.kt
@@ -0,0 +1,52 @@
+package ru.netology.nmedia.dto
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class PostEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long,
+    val author: String,
+    val content: String,
+    val published: String,
+    val likes: Int = 0,
+    val share: Int = 0,
+    val chat: Int = 0,
+    val views: Int = 0,
+    val videoUrl: String,
+    val likedByMe: Boolean
+) {
+    companion object {
+        fun fromPost(post: Post): PostEntity =
+            with(post) {
+                PostEntity(
+                    id,
+                    author,
+                    content,
+                    published,
+                    likes,
+                    share,
+                    chat,
+                    views,
+                    videoUrl,
+                    likedByMe
+                )
+            }
+    }
+
+}
+    fun PostEntity.toPost(): Post =
+        Post(
+            id,
+            author,
+            content,
+            published,
+            likes,
+            share,
+            chat,
+            views,
+            videoUrl,
+            likedByMe
+        )
+
